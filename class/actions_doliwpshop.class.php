d6e277dc4d262279a1831e62ebd5ee4488f574e5
evarisk-theo <68421590+evarisk-theo@users.noreply.github.com>
2020-07-24 14:41:32 +0200

    Update actions_doliwpshop.class.php

--- class/actions_doliwpshop.class.php
+++ class/actions_doliwpshop.class.php
@@ -21,9 +21,9 @@
  * \brief   Hook on new actions for connected Dolibarr and WPshop
  */
 
-require_once DOL_DOCUMENT_ROOT . '/custom/doliwpshop/lib/api_doliwpshop.class.php';
-require_once DOL_DOCUMENT_ROOT . '/custom/doliwpshop/class/product_doliwpshop.class.php';
-require_once DOL_DOCUMENT_ROOT . '/custom/doliwpshop/class/thirdparty_doliwpshop.class.php';
+require_once dol_include_once . '/custom/doliwpshop/lib/api_doliwpshop.class.php';
+require_once dol_include_once . '/custom/doliwpshop/class/product_doliwpshop.class.php';
+require_once dol_include_once . '/custom/doliwpshop/class/thirdparty_doliwpshop.class.php';
 
 /**
  * Class ActionsDoliWPshop
