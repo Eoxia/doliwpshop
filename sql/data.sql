80359ed3bdbc096dd767c685b7b0a414fa6c511d
Nicolas <nicolas@eoxia.com>
2021-05-20 18:08:25 +0200

    [SQL] add : trigger element

---
+++ sql/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO llx_c_action_trigger (elementtype, code, label, description) VALUES ('paiement', 'PAYMENTONLINE_PAYMENT_OK', 'Online payment trigger', 'Executed when a online payment is triggered');
