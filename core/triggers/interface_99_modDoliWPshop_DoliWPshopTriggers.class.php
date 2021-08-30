46ea5a149d0277af815bdf9e019c6d1f67a69435
Nicolas <nicolas@eoxia.com>
2021-08-30 11:58:17 +0200

    [Trigger] add : translate trigger for use module trad

--- core/triggers/interface_99_modDoliWPshop_DoliWPshopTriggers.class.php
+++ core/triggers/interface_99_modDoliWPshop_DoliWPshopTriggers.class.php
@@ -304,17 +304,19 @@ class InterfaceDoliWPshopTriggers extends DolibarrTriggers
 
 				$productLang = new ProductLang($this->db);
 
-				$arrayProductLangs = $productLang->fetchAll('', 't.rowid', 0, 0, array('t.fk_product'=>$object->id), '');
+				$arrayProductLangs     = $productLang->fetchAll('', 't.rowid', 0, 0, array('t.fk_product'=>$object->id), '');
 				$lastArrayProductLangs = end($arrayProductLangs);
 
-				$lastArrayProductLangsData = array();
-				$lastArrayProductLangsData['lang'] = $lastArrayProductLangs->lang;
-				$lastArrayProductLangsData['label'] = $lastArrayProductLangs->label;
+				$lastArrayProductLangsData                = array();
+				$lastArrayProductLangsData['lang']        = $lastArrayProductLangs->lang;
+				$lastArrayProductLangsData['label']       = $lastArrayProductLangs->label;
 				$lastArrayProductLangsData['description'] = $lastArrayProductLangs->description;
+				$lastArrayProductLangsData['fk_product']  = $lastArrayProductLangs->fk_product;
+				$lastArrayProductLangsData['wpshop_id']   = $object->array_options['options__wps_id'];
 
 				$wpmlPostIdTranslated = WPshopAPI::post('/wp-json/wpshop/v2/wpml_insert_data',$lastArrayProductLangsData);
 
-				$lastArrayProductLangs->array_options['wpshopidtradmultilangs'] = $wpmlPostIdTranslated['data'];
+				$lastArrayProductLangs->array_options['wpshopidtradmultilangs']  = $wpmlPostIdTranslated['data'];
 				$lastArrayProductLangs->array_options['wpshopurltradmultilangs'] = $conf->global->WPSHOP_URL_WORDPRESS . '/?post_type=wps-product&p=' . $wpmlPostIdTranslated['data'];
 
 				$lastArrayProductLangs->insertExtraFields();
