dc544d00497ea62fc5482b5ca23632cbac8ec729
Laurent Magnin <laurent1magnin@gmail.com>
2026-09-01 23:06:29 +0200

    feat: add WPSHOP_DEFAULT_PRODUCT_STATUS setting and set default to draft

--- core/modules/modDoliWPshop.class.php
+++ core/modules/modDoliWPshop.class.php
@@ -228,7 +228,8 @@ class modDoliWPshop extends DolibarrModules {
 		include_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
 		$extra_fields = new ExtraFields( $this->db );
 		
-		$extra_fields->addExtraField( '_wps_status', $langs->trans("WPshopStatus"), 'select', 999, '', 'product', 0, 0, 'publish', array('options' => array('publish'=> 'publish', 'draft' => 'draft') ) );
+		$default_status = !empty($conf->global->WPSHOP_DEFAULT_PRODUCT_STATUS) ? $conf->global->WPSHOP_DEFAULT_PRODUCT_STATUS : 'draft';
+		$extra_fields->addExtraField( '_wps_status', $langs->trans("WPshopStatus"), 'select', 999, '', 'product', 0, 0, $default_status, array('options' => array('publish'=> 'publish', 'draft' => 'draft') ) );
 		$extra_fields->addExtraField( '_wps_id', 'WPshop ID', 'int', 1000, '', 'product', 1, 0,'','', 0,'','1' );
 
 		$extra_fields->addExtraField( 'firstname', 'Firstname', 'varchar', 2, '255', 'thirdparty' );
